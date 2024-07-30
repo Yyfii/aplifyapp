b586e2cc7a517ae4edb9961d5adac2467ef2b208
Yyfii <anasoaresnunes365@gmail.com>
2024-07-30 18:32:03 -0300

    config: configuring the user aws cli amplify

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import logo from './logo.svg';
 
 function App() {
   return (
+    
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -20,5 +21,6 @@ function App() {
     </div>
   );
 }
+
 
 export default App;
